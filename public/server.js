98501bcdbe46f27ca5cafb6c0bd1a12bd108ff4d
Yechan <yechankim0531@gmail.com>
2024-07-03 16:22:33 +0900

    updated server.js

--- public/server.js
+++ public/server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src'))); // Serve static files from the 'src' directory
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/home.html');
-    });
+    res.sendFile(path.join(__dirname, 'src', 'home.html')); // Correct path concatenation
+});
 
-    app.get('/', function(req, res){
-        res.sendFile(__dirname + '/public/home.html');
-        });    
+app.get('/question', function(req, res){
+    res.sendFile(path.join(__dirname, 'src', 'question.html')); // Serve question.html for the /question route
+});
+
+app.get('/user', function(req, res){
+    res.sendFile(path.join(__dirname, 'src', 'user.html')); // Serve user.html for the /user route
+});
 
 app.listen(8080, () => {
-    console.log(`Server is listening`);
-});    
+    console.log('Server is listening on port 8080');
+});
