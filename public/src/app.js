14907145ecabfd67ec7c498f5eb4aa19a38c5fba
Yechan <yechankim0531@gmail.com>
2024-07-03 16:42:35 +0900

    created question.html

---
+++ public/src/app.js
@@ -0,0 +1,26 @@
+var startButton = document.getElementById('startSurvey');
+var nextBtn = document.getElementById('nextBtn');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button element by its ID
+    
+    
+    // Add a click event listener to the button
+    startButton.addEventListener('click', function() {
+        // Redirect to the survey.html page when the button is clicked
+        window.location.href = 'user.html';
+    });
+
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button element by its ID
+
+
+    nextBtn.addEventListener('click', function() {
+        // Redirect to the survey.html page when the button is clicked
+        window.location.href = 'question.html';
+    });
+});
